9bb5e6304bb83be5d8795ad6afc60aa0ea95ff0c
Michael Petersen <railfan@drgw.net>
2017-03-10 20:33:20 -0700

    Initial commit

---
+++ src/mrbw-xpressnet.c
@@ -0,0 +1,136 @@
+/*************************************************************************
+Title:    MRBW-XPRESSNET MRBee XpressNet Interface
+Authors:  Michael D. Petersen <railfan@drgw.net>
+          Nathan D. Holmes <maverick@drgw.net>
+File:     $Id: $
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2017 Michael Petersen & Nathan Holmes
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*************************************************************************/
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/wdt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include "mrbee.h"
+
+#define MRBUS_TX_BUFFER_DEPTH 16
+#define MRBUS_RX_BUFFER_DEPTH 16
+
+MRBusPacket mrbusTxPktBufferArray[MRBUS_TX_BUFFER_DEPTH];
+MRBusPacket mrbusRxPktBufferArray[MRBUS_RX_BUFFER_DEPTH];
+
+uint8_t mrbus_dev_addr = 0;
+
+void createVersionPacket(uint8_t destAddr, uint8_t *buf)
+{
+	buf[MRBUS_PKT_DEST] = destAddr;
+	buf[MRBUS_PKT_SRC] = mrbus_dev_addr;
+	buf[MRBUS_PKT_LEN] = 15;
+	buf[MRBUS_PKT_TYPE] = 'v';
+	buf[6]  = MRBUS_VERSION_WIRELESS;
+	// Software Revision
+	buf[7]  = 0xFF & ((uint32_t)(GIT_REV))>>16; // Software Revision
+	buf[8]  = 0xFF & ((uint32_t)(GIT_REV))>>8; // Software Revision
+	buf[9]  = 0xFF & (GIT_REV); // Software Revision
+	buf[10]  = HWREV_MAJOR; // Hardware Major Revision
+	buf[11]  = HWREV_MINOR; // Hardware Minor Revision
+	buf[12] = 'C';
+	buf[13] = 'S';
+	buf[14] = 'T';
+}
+
+volatile uint8_t ticks;
+volatile uint16_t decisecs = 0;
+
+void initialize100HzTimer(void)
+{
+	// Set up timer 0 for 100Hz interrupts
+	TCNT0 = 0;
+	OCR0A = 0x6C;  // FIXME for 20MHz
+	ticks = 0;
+	decisecs = 0;
+	TCCR0A = _BV(WGM01);
+	TCCR0B = _BV(CS02) | _BV(CS00);
+	TIMSK0 |= _BV(OCIE0A);
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+	if (++ticks >= 10)  // 100ms
+	{
+		ticks = 0;
+		decisecs++;
+	}
+}
+
+void init(void)
+{
+	// Clear watchdog (in the case of an 'X' packet reset)
+	MCUSR = 0;
+#ifdef ENABLE_WATCHDOG
+	// If you don't want the watchdog to do system reset, remove this chunk of code
+	wdt_reset();
+	WDTCSR |= _BV(WDE) | _BV(WDCE);
+	WDTCSR = _BV(WDE) | _BV(WDP2) | _BV(WDP1); // Set the WDT to system reset and 1s timeout
+	wdt_reset();
+#else
+	wdt_reset();
+	wdt_disable();
+#endif
+
+	initialize100HzTimer();
+}
+
+int main(void)
+{
+	uint8_t txBuffer[MRBUS_BUFFER_SIZE];
+
+	init();
+
+	wdt_reset();
+
+	// Initialize MRBus core
+	mrbusPktQueueInitialize(&mrbeeTxQueue, mrbusTxPktBufferArray, MRBUS_TX_BUFFER_DEPTH);
+	mrbusPktQueueInitialize(&mrbeeRxQueue, mrbusRxPktBufferArray, MRBUS_RX_BUFFER_DEPTH);
+	mrbeeInit();
+
+	sei();	
+
+	wdt_reset();
+
+	// Fire off initial reset version packet
+	createVersionPacket(0xFF, txBuffer);
+	mrbusPktQueuePush(&mrbeeTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+
+	wdt_reset();
+
+	while(1)
+	{
+		wdt_reset();
+
+		if (mrbusPktQueueDepth(&mrbeeTxQueue))
+		{
+			wdt_reset();
+			mrbeeTransmit();
+		}
+
+	}
+
+}
+
+
